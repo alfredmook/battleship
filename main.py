0e341aa4779f19eda801b6b7df883d78930cf457
alfred <261239787+alfredmook@users.noreply.github.com>
2026-04-14 03:12:01 +0000

    hi

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def create_grid():
         grid.append(templist)
     return grid
 
+
 def initialize_grid():
     """
     Creates a 10x10 grid for player ships, player attacks,computer ships, computer attacks
@@ -46,6 +47,7 @@ def place_ships(grid):
     ships = [Battleship, Cruiser, Destroyer] 
     
     valid_placement = False
+    ship_count = 0
     for ship in ships:
         valid_placement = False
         while not valid_placement:
@@ -68,27 +70,22 @@ def place_ships(grid):
                 continue
             
             for i in range(ship[0]):
+                ship_count += 1
                 if orientation == 0:
                     grid[y][x+i] = ship[1]
                 else:
                     grid[y+i][x] = ship[1]
     
-    return grid
+    return grid, ship_count
+
 
 def display_grid(grid):
-    print(" 0 1 2 3 4 5 6 7 8 9")
+    print("  0 1 2 3 4 5 6 7 8 9")
     for i,row in enumerate(grid):
-        print(str(i)+" ".join(row))
+        print(str(i)," ".join(row))
 
 
-player_ship_grid, player_attack_grid, computer_ship_grid, computer_attack_grid = initialize_grid()
-
-player_ship_grid = place_ships(player_ship_grid)
-computer_ship_grid = place_ships(computer_ship_grid)
-
- #PLAYER
-turns = 30
-def attack(put_x,put_y,opponent_ship_grid,self_attack_grid):
+def attack(put_x, put_y, opponent_ship_grid, self_attack_grid):
     cell = opponent_ship_grid[put_y][put_x]
     if cell != "~":
         print("Hit ship", cell)
@@ -100,53 +97,76 @@ def attack(put_x,put_y,opponent_ship_grid,self_attack_grid):
         opponent_ship_grid[put_y][put_x] = "O"
         self_attack_grid[put_y][put_x] = "#"
         return False
-player_ship_status = 9
-computer_ship_status = 9
+    
+
 #Total cells ships B,C and D have god forgive me
+#say wallahi we hardcoding
 #Alfred please make this better danke schoen
-win = False
-for i in range(turns):
-    print("Player's turn:")
-    print("Ship grid: ")
-    #display_grid(computer_ship_grid)
-    display_grid(player_ship_grid)
-    success = True
-    while success:
-        print("Attack grid: ")
-        display_grid(player_attack_grid)
-        while True:
-            player_put_x, player_put_y = input("Input target coordinates in the format x,y: ").split(",")
-            if not (player_put_x.isdigit() and player_put_y.isdigit()):
-                print("Bro ts not valid frfr")
-            else:
-                player_put_x = int(player_put_x)
-                player_put_y = int(player_put_y)
-                if 0 <= player_put_x <= 9 and 0 <= player_put_y <= 9:
-                    if player_attack_grid[player_put_y][player_put_x] != "~":
-                        print("Cell was already selected before brodie select again")
-                    else:
-                        break #All checks passed can exit
-        success = attack(player_put_x,player_put_y,computer_ship_grid,player_attack_grid)
-        if success:
-            computer_ship_status -= 1
-            if computer_ship_status==0:
-                win = True
-                break
-            print("Player gets to go again")
-    if win:
-        break
-    print("CPU's turn:")
-    while True:
-        computer_put_x, computer_put_y = random.randint(0, 9), random.randint(0, 9)
-        if computer_attack_grid[computer_put_y][computer_put_x] == "~":
+def game(player_ship_grid, computer_ship_grid, player_attack_grid, 
+         computer_attack_grid, player_ship_status, computer_ship_status):
+    
+    win = False
+    turns = 30
+    for i in range(turns):
+        print("Player's turn:")
+        print("Ship grid: ")
+        display_grid(computer_ship_grid)
+        display_grid(player_ship_grid)
+        success = True
+        while success and computer_ship_status:
+            print("Attack grid: ")
+            display_grid(player_attack_grid)
+            while True:
+                
+                player_put = input("Input target coordinates in the format x,y: ").strip(" ").split(",")
+                if len(player_put) != 2:
+                    print("put only 2 coords bro is it that hard")
+                    continue
+
+                player_put_x, player_put_y = player_put
+                if not (player_put_x.isdigit() and player_put_y.isdigit()):
+                    print("Bro ts not valid frfr")
+                elif not (0 <= int(player_put_x) <= 9) or not (0 <= int(player_put_y) <= 9):
+                    print("put in numbers la")
+                elif player_attack_grid[int(player_put_y)][int(player_put_x)] != "~":
+                    print("Cell was already selected before brodie select again")
+                else:
+                    player_put_x = int(player_put_x)
+                    player_put_y = int(player_put_y)
+                    break #All checks passed can exit 
+                    #woohoo
+
+            success = attack(player_put_x, player_put_y, computer_ship_grid, player_attack_grid)
+            if success:
+                computer_ship_status -= 1
+                print("Player gets to go again")
+        
+        if computer_ship_status == 0:
+            win = True
             break
-    if attack(computer_put_x,computer_put_y,player_ship_grid,computer_attack_grid):
-        player_ship_status -= 1
-    if player_ship_status == 0:
-        break
-if win:
-    print("Yay you win you sunk all them ships")
-else:
-    print("Boo you lost")
+
+        print("CPU's turn:")
+        while True:
+            computer_put_x, computer_put_y = random.randint(0, 9), random.randint(0, 9)
+            if computer_attack_grid[computer_put_y][computer_put_x] == "~":
+                break
+        if attack(computer_put_x,computer_put_y,player_ship_grid,computer_attack_grid):
+            player_ship_status -= 1
+
+        if not player_ship_status:
+            break
+
+    if win:
+        print("Yay you win you sunk all them ships")
+    else:
+        print("Boo you lost")
+
+player_ship_grid, player_attack_grid, computer_ship_grid, computer_attack_grid = initialize_grid()
+player_ship_grid, player_ship_status = place_ships(player_ship_grid)
+computer_ship_grid, computer_ship_status = place_ships(computer_ship_grid)
+game(player_ship_grid, computer_ship_grid, computer_attack_grid, 
+     player_attack_grid, player_ship_status, computer_ship_status)
+
+
 
 
